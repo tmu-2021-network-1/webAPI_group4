a478d46d0b2ec80dfd65eed544477f1d991454ea
muto15 <56058496+muto15@users.noreply.github.com>
2021-05-21 10:03:21 +0900

    Update apidata.js

--- docs/apidata.js
+++ docs/apidata.js
@@ -2,7 +2,7 @@
 // const id = '1BpGnuwC4lZf9G2yFyiSrxbJuGO8gviV8mr-I2D3x4vA';
 // const sheet = 'Studio';
 // const endpoint = `${uri}?id=${id}&sheet=${sheet}`;
-const endpoint = 'https://collectionapi.metmuseum.org/public/collection/v1/departments';
+const endpoint = 'https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search?q=%E5%88%9D%E9%9F%B3%E3%83%9F%E3%82%AF&targets=title&fields=contentId,title,viewCounter&filters[viewCounter][gte]=10000&_sort=-viewCounter&_offset=0&_limit=3&_context=apiguide';
 
 const renderJson = (json) => {
   const studios = json.recor;
