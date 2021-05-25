3fa2f7334da4b286c34dbdf5bbeb8e5e7d484acc
Yuka1105 <56023851+Yuka1105@users.noreply.github.com>
2021-05-25 23:28:08 +0900

    googleシートの導入やろうとしたけど無理でした...

---
+++ docs/script.js
@@ -0,0 +1,4 @@
+const uri = 'https://script.google.com/macros/s/AKfycbxyacpN8y4nxSAnU0Eji6E_rBRDFTY7YoWWFa0clY5ELRhskgpt/exec';
+const id = '11aa0JNhBTdEbj_HnwAVjc2aUwr8FPqgAtSWW89EhGH4';
+const sheet = 'All';
+const endpoint = `${uri}?id=${id}&sheet=${sheet}`;
