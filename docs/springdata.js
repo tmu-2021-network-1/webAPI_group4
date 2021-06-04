fcc7464105bfe9880baf88b4fa1730877a57d861
muto15 <sayaka490@ybb.ne.jp>
2021-06-04 15:24:25 +0900

    米田ファイル加えた

--- docs/springdata.js
+++ docs/springdata.js
@@ -6,16 +6,32 @@ const endpoint = 'https://script.google.com/macros/s/AKfycbxldXF6kuQUSbhI6M0pD5M
 
 const renderJson = (json) => {
 
-  var  haru = [0,2,5,99];
+  var  haru = [8,14,32,34,72];
   const utaHaruDiv = document.createElement('div');
+  utaHaruDiv.className = 'karuta';
   for(var i = 0;i<haru.length;i++){
-  const utaHaruone = document.createElement("p");
-  utaHaruone.className = 'utaharuone';
-  utaHaruone.textContent = json[haru[i]].one;
-  utaHaruDiv.appendChild(utaHaruone);
+    const utaHaruone = document.createElement("p");
+    const utaHarutwo = document.createElement("p");
+    const utaHaruthree = document.createElement("p");
+    const utaHarufour = document.createElement("p");
+    const utaHarufive = document.createElement("p");
+    // utaHaruone.className = 'utaharuone';
+    // utaHarutwo.className = 'utaharutwo';
+    // utaHaruthree.className = 'utaharuthree';
+    // utaHarufour.className = 'utaharufour';
+    // utaHarufive.className = 'utaharufive';
+    utaHaruone.textContent = json[haru[i]].one;
+    utaHarutwo.textContent = json[haru[i]].two;
+    utaHaruthree.textContent = json[haru[i]].three;
+    utaHarufour.textContent = json[haru[i]].four;
+    utaHarufive.textContent = json[haru[i]].five;
+    utaHaruDiv.appendChild(utaHaruone);
+    utaHaruDiv.appendChild(utaHarutwo);
+    utaHaruDiv.appendChild(utaHaruthree);
+    utaHaruDiv.appendChild(utaHarufour);
+    utaHaruDiv.appendChild(utaHarufive);
     console.log(json[haru[i]].one);
     console.log(json[haru[i]].author);
-    
   }
   document.getElementById('haruuta').appendChild(utaHaruDiv);
 }
